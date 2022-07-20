9d3b9371802b115391068d800dfa8b5c661ff089
Seb <gillisl22@outlook.com>
2022-07-20 19:56:19 -0400

    portfolio images, and links

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,24 +1,24 @@
 import React from 'react';
+import img1 from '../../img/goldDevs.png';
+import img2 from '../../img/zelda.jpg'
+import img3 from '../../img/jade.png'
 
 export default function Portfolio() {
   return (
     <div className='backgroundColor'>
-      <h1>Portfolio Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <div className='row'>
+
+        <div className='col'>
+          <a href="https://protected-peak-08029.herokuapp.com/"><img className='portfolioImage ' id="A image of me"src={img1}/></a>
+          <a href="https://ydennekrf.github.io/Node-Aero/"><img className='portfolioImage ' id="A image of me"src={img2}/></a>
+        </div>
+
+        <div className='col'>
+            <a href="https://mighty-hamlet-13053.herokuapp.com/"><img className='portfolioImage ' id="A image of me"src={img3}/></a>
+            
+        </div>
+
+      </div>
     </div>
   );
 }
