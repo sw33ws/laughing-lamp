3ac1ad77baf47da2b7ec26b9aec77691c033b672
Seb <gillisl22@outlook.com>
2022-07-20 20:43:14 -0400

    resume css

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,28 +3,32 @@ import React from 'react';
 export default function Resume() {
   return (
     <div className='backgroundColor'>
-      <h1>My Resume</h1>
-      <h3>FrontEnd</h3>
-      <ul>
-        <li>HTML</li>
-        <li>Css</li>
-        <li>Javascript</li>
-        <li>React</li>
-        <li>bootstrap</li>
-        <li>TailWind</li>
-      </ul>
+      <div className='Resume'>
+        <h1>My Resume</h1>
+        <div className='Resume-text'>
+          <h3>FrontEnd</h3>
+          <ul>
+            <li>HTML</li>
+            <li>Css</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>bootstrap</li>
+            <li>TailWind</li>
+          </ul>
 
-      <br></br>
+          <br></br>
 
-      <h3>FrontEnd</h3>
-      <ul>
-        <li>Api's</li>
-        <li>Node Js</li>
-        <li>Express</li>
-        <li>Mysql</li>
-        <li>MongoDb</li>
-        <li>GraphQL</li>
-      </ul>
+          <h3>BackEnd</h3>
+          <ul>
+            <li>Api's</li>
+            <li>Node Js</li>
+            <li>Express</li>
+            <li>Mysql</li>
+            <li>MongoDb</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+      </div>
 
     </div>
   );
